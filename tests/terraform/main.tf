2262b0f029df8ec90c917e7def28c0987a570f26
Diego Heer <diegojonathanheer@gmail.com>
2025-02-16 13:42:08 +0000

    Tests/Terraform|Feature: build own docker file with sshd and systemd enabled, for testing purposes

--- tests/terraform/main.tf
+++ tests/terraform/main.tf
@@ -1,11 +1,21 @@
 resource "docker_image" "test_server" {
-  name = "takeyamajp/ubuntu-sshd:latest"
+  name = "test_server_ubuntu"
+  build {
+    context = "."
+    tag     = ["test_server:ubuntu"]
+  }
 }
 
 resource "docker_container" "test_server" {
   name  = "test_server"
   image = docker_image.test_server.image_id
+  privileged = true
 
+  volumes {
+    container_path = "/sys/fs/cgroup"
+    host_path      = "/sys/fs/cgroup"
+    read_only      = true
+  }
   ports {
     internal = "22"
     external = var.ansible_port
--- tests/terraform/outputs.tf
+++ tests/terraform/outputs.tf
@@ -1,6 +1,6 @@
 output "root_ssh_access" {
   description = "Command to SSH using root user to the test server"
-  value = "sshpass -p ${var.ansible_password} ssh ${var.ansible_username}@${var.ansible_host} -p ${var.ansible_port}"
+  value       = "sshpass -p ${var.ansible_password} ssh ${var.ansible_username}@${var.ansible_host} -p ${var.ansible_port}"
 }
 
 output "local_ssh_access" {
